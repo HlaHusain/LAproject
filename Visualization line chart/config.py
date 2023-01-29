ff3a068485e47ca6fe280d8a144bfc8a8148043c
Ruidan LIU <57582693+dandan950@users.noreply.github.com>
2023-01-29 22:53:53 +0100

    Delete Visualization line chart directory

--- Visualization line chart/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-CONNECTION_STRING = os.environ.get(
-    "DB", 'mongodb+srv://databits:group5@cluster0.vt9xqx9.mongodb.net/?retryWrites=true&w=majority')
-
-
-# AUTH_CONFIG = {
-#     # "SECRET_KEY": "ASDKJZMVM!@$?!@*^&$%@#$"
-# }
